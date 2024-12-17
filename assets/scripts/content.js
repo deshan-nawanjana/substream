cfc68ff209e97c3dc22718e8ff0dec474aa8f745
Deshan Nawanjana <deshan.uok@gmail.com>
2024-12-17 21:15:55 +0530

    Initial Commit

---
+++ assets/scripts/content.js
@@ -0,0 +1,305 @@
+function sendMessage(type, data) {
+  chrome.runtime.sendMessage({ type, data }).catch(() => { })
+}
+
+function toSeconds(time) {
+  // split units
+  const [hours, minutes, seconds] = time.split(":")
+  // split seconds
+  const [secs, mill] = seconds.split(",")
+  // convert to seconds
+  return (
+    parseInt(hours, 10) * 3600 +
+    parseInt(minutes, 10) * 60 +
+    parseInt(secs, 10) +
+    parseInt(mill, 10) / 1000
+  )
+}
+
+function parseLines(text) {
+  // split into text lines
+  const lines = text.split(/\r?\n/)
+  // output array
+  const output = []
+  // current line data
+  let current = { from: 0, to: 0, text: "" }
+  // for each line
+  for (let i = 0; i < lines.length; i++) {
+    // trim current line
+    const line = lines[i].trim()
+    // check for line format
+    if (/^\d+$/.test(line)) {
+      // reset current line on line number
+      current = { from: 0, to: 0, text: "" }
+    } else if (line.includes("-->")) {
+      // parse time range
+      const [start, end] = line.split("-->").map(time => time.trim())
+      // set on current time values
+      current.from = toSeconds(start)
+      current.to = toSeconds(end)
+    } else if (line) {
+      // concat into line text
+      current.text = (current.text ? current.text + "<br>" : "") + line
+    } else if (current.text) {
+      // push current line to output
+      output.push(current)
+    }
+  }
+  return output
+}
+
+
+const applyStyle = (element, current, history = null) => {
+  // get current keys
+  const keys = Object.keys(current)
+  // for each key
+  for (let i = 0; i < keys.length; i++) {
+    // current key and value
+    const key = keys[i]
+    const value = current[key]
+    // check with history value
+    if (!history || history[key] !== value) {
+      // set value on element
+      element.style[key] = typeof value === "number"
+        ? `${value}px` : value
+      // update history
+      if (history) { history[key] = value }
+    }
+  }
+}
+
+// player data
+const data = {
+  // init state
+  init: false,
+  // target element index
+  target: null,
+  // file name
+  name: "none",
+  // subtitle lines
+  lines: []
+}
+
+// time data
+const time = {
+  // current playback time
+  current: 0,
+  // current playback duration
+  duration: 0,
+  // sync offset
+  sync: 0
+}
+
+// display element data
+const overlay = {
+  // outer element
+  outer: {
+    element: document.createElement("div"),
+    style: {
+      // avoid inheritance
+      all: "initial",
+      // dimensions
+      width: 0, height: 0,
+      // position
+      left: 0, top: 0,
+      // alignment
+      justifyContent: "center",
+      alignItems: "end",
+      // spacing
+      paddingLeft: 65,
+      paddingRight: 65,
+      paddingTop: 86,
+      paddingBottom: 86,
+      // default
+      pointerEvents: "none",
+      position: "fixed",
+      display: "flex",
+      boxSizing: "border-box"
+    }
+  },
+  // inner element
+  inner: {
+    element: document.createElement("div"),
+    style: {
+      // avoid inheritance
+      all: "initial",
+      // captions
+      fontSize: 27,
+      fontWeight: "normal",
+      textAlign: "center",
+      // background
+      backgroundColor: "rgba(0, 0, 0, 0.0)",
+      // default
+      pointerEvents: "none",
+      color: "rgba(255, 255, 255, 1)",
+      borderRadius: 10,
+      padding: "8px 12px",
+      fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+    }
+  }
+}
+
+// get elements
+const outer = overlay.outer.element
+const inner = overlay.inner.element
+// get style
+const outerStyle = overlay.outer.style
+const innerStyle = overlay.inner.style
+
+// set element ids
+outer.id = "-ext-sub-stream-overlay-outer"
+inner.id = "-ext-sub-stream-overlay-inner"
+
+// apply element styles
+applyStyle(outer, outerStyle)
+applyStyle(inner, innerStyle)
+
+// append inner into outer
+outer.appendChild(inner)
+
+const init = () => {
+  // set init flag
+  data.init = true
+  // start update frame loop
+  update()
+}
+
+const update = () => {
+  // request next frame
+  if (data.init) { requestAnimationFrame(update) }
+  // get current video element
+  const video = data.target
+  // check video element
+  if (video) {
+    // update current time and duration
+    time.current = video.currentTime
+    time.duration = video.duration
+    // get current type by sync
+    const current = time.current - time.sync
+    // find line by current time
+    const line = data.lines.find(line => (
+      // line between start time and end tme
+      line.from <= current && line.to >= current
+    ))
+    // get text to display
+    const text = line ? line.text : ""
+    // update inner element content
+    if (inner.innerHTML !== text) { inner.innerHTML = text }
+    // display inner by text
+    inner.style.display = text !== "" ? "block" : "none"
+    // get video bounding rect
+    const rect = video.getBoundingClientRect()
+    // update outer position
+    applyStyle(outer, {
+      width: rect.width,
+      height: rect.height,
+      left: rect.left,
+      top: rect.top
+    }, outerStyle)
+    // get parent element
+    const parent = video.parentElement
+    // check overlay on parent
+    if (!parent.querySelector("#" + outer.id)) {
+      parent.appendChild(outer)
+    }
+  } else {
+    inner.style.display = "none"
+  }
+}
+
+// method to update video element
+const onElement = () => {
+  // get all video elements
+  const elements = Array.from(document.querySelectorAll("video"))
+  // get each duration
+  const durations = elements.map(item => item.duration)
+    .filter(item => !isNaN(item))
+    .filter(item => item > 10)
+  // return if no durations
+  if (durations.length === 0) { return }
+  // find maximum duration
+  const maximum = Math.max(...durations)
+  // return if current target duration is the same
+  if (data.target && data.target.duration === maximum) { return }
+  // set as target
+  data.target = elements.find(item => item.duration === maximum)
+}
+
+// method to upload subtitle file
+const onUpload = () => {
+  return new Promise(resolve => {
+    // create file input
+    const input = document.createElement("input")
+    // set input options
+    input.type = "file"
+    input.accept = ".srt"
+    input.multiple = false
+    // file input listener
+    input.addEventListener("input", () => {
+      // get file
+      const file = input.files[0]
+      // create file reader
+      const reader = new FileReader()
+      // loaded event listener
+      reader.addEventListener("load", () => {
+        // update file name
+        data.name = file.name
+        // parse subtitle lines
+        data.lines = parseLines(reader.result)
+        // resolve callback
+        resolve()
+      })
+      // read file as text
+      reader.readAsText(file)
+    })
+    // trigger file select
+    input.click()
+  })
+}
+
+chrome.runtime.onMessage.addListener(async (message, _s, callback) => {
+  // get message data
+  const action = message.action
+  const payload = message.payload
+  // update target element
+  onElement()
+  // switch action
+  if (action === "update") {
+    // update name
+    if ("name" in payload) { data.name = payload.name }
+    // update lines
+    if ("lines" in payload) { data.lines = payload.lines }
+    // update sync offset
+    if ("sync" in payload) { time.sync = payload.sync }
+    // update outer style
+    if ("outer" in payload) { applyStyle(outer, payload.outer, outerStyle) }
+    // update inner style
+    if ("inner" in payload) { applyStyle(inner, payload.inner, innerStyle) }
+  } else if (action === "upload") {
+    // upload subtitle file
+    await onUpload()
+  }
+  // switch action
+  if (action === "stop") {
+    // reset initiation
+    data.init = false
+    // hide text element
+    inner.style.display = "none"
+    // reset name
+    data.name = "none"
+    // reset lies
+    data.lines = []
+    // callback all data
+    sendMessage("data", { data, time, overlay })
+  } else if (action === "time") {
+    // callback time data
+    sendMessage("time", { time })
+  } else {
+    // init player if not initiated
+    if (!data.init) { init() }
+    // callback all data
+    sendMessage("data", { data, time, overlay })
+  }
+  // callback as success
+  callback(true)
+})
---
+++ index.js
@@ -0,0 +1,304 @@
+// query selector helpers
+const qs = selector => document.querySelector(selector)
+const qa = selector => document.querySelectorAll(selector)
+
+function toTimeString(seconds) {
+  // divide into units
+  const h = Math.floor(seconds / 3600)
+  const m = Math.floor((seconds % 3600) / 60)
+  const s = parseInt(seconds % 60)
+  // format pad start
+  const ph = h.toString().padStart(2, "0")
+  const pm = m.toString().padStart(2, "0")
+  const ps = s.toString().padStart(2, "0")
+  // return string
+  return `${ph}:${pm}:${ps}`
+}
+
+// pages to avoid from messaging
+const exceptions = [
+  "about:",
+  "chrome://",
+  "chrome-extension://",
+  "https://chrome.google.com/webstore"
+]
+
+const sendMessage = (action, payload = null) => {
+  return new Promise(resolve => {
+    // create message
+    const message = { action, payload }
+    // query active current tabs
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      // return if no tsbs
+      if (tabs.length === 0) { return resolve(false) }
+      // get active tab
+      const activeTab = tabs[0]
+      // return if no url
+      if (!activeTab.url) { return resolve(false) }
+      // return if active tab is a restricted page
+      if (exceptions.some(part => activeTab.url.startsWith(part))) {
+        return resolve(false)
+      }
+      // send message to get data
+      chrome.tabs.sendMessage(activeTab.id, message).then(data => {
+        // resolve data
+        resolve(data)
+      }).catch(() => resolve(false))
+    })
+  })
+}
+
+// app state object
+const states = { tab: "upload", lines: [], time: null }
+
+// message callback handler
+chrome.runtime.onMessage.addListener(message => {
+  // return if no message
+  if (!message) { return }
+  // get message type
+  const type = message.type
+  // get message data
+  const data = message.data.data
+  const time = message.data.time
+  const overlay = message.data.overlay
+  // switch by type
+  if (type === "data") {
+    // set initial lines
+    if (states.tab === "upload") { onTiming(data.lines) }
+    // update file name
+    qs("[data-name]").setAttribute("data-name", data.name)
+    qs(".upload-preview-name").innerHTML = data.name
+    // update settings inputs
+    if (states.tab !== "settings") {
+      // set style data
+      const outerStyle = overlay.outer.style
+      const innerStyle = overlay.inner.style
+      // spacing
+      qs("#spacing-x").value = outerStyle.paddingLeft
+      qs("#spacing-y").value = outerStyle.paddingTop
+      // position
+      qs("[data-position]").setAttribute(
+        "data-position",
+        `${outerStyle.alignItems}-${outerStyle.justifyContent}`
+      )
+      // font size
+      qs("#font-size").value = innerStyle.fontSize
+      // font weight
+      qs("[data-weight]").setAttribute("data-weight", innerStyle.fontWeight)
+      // background opacity
+      qs("#background-opacity").value =
+        innerStyle.backgroundColor.split(/[,)]/g)[3] * 100
+    }
+    // update timing input
+    if (states.tab !== "timing") {
+      // sync amount
+      qs("#sync").value = message.data.time.sync
+    }
+    // store current overlay settings
+    localStorage.setItem("settings", JSON.stringify(overlay))
+  } else if (type === "time") {
+    // update timing
+    onTimingUpdate(time)
+    // update timing input
+    if (states.tab !== "timing") {
+      // sync amount
+      qs("#sync").value = message.data.time.sync
+    }
+  }
+  // update preview time
+  qs(".upload-preview-time").innerHTML = `
+    ${toTimeString(time.current)} / ${toTimeString(time.duration)}
+  `
+})
+
+// upload tray click event
+qs(".upload-tray").addEventListener("click", () => {
+  // trigger upload file
+  sendMessage("upload")
+})
+
+// for each tab button
+Array.from(qa(".tab")).forEach(tab => {
+  // set tab id into app
+  tab.addEventListener("click", () => {
+    // set tab id on states
+    states.tab = tab.classList[1]
+    // update tab attribute
+    document.body.setAttribute("data-tab", states.tab)
+  })
+})
+
+const onStop = () => {
+  // stop current captions
+  sendMessage("stop")
+}
+
+const onTiming = lines => {
+  // update lines
+  states.lines = lines
+  // get lines container
+  const container = qs(".timing-lines")
+  // reset container
+  container.innerHTML = ""
+  // for each line
+  for (let i = 0; i < lines.length; i++) {
+    // current line
+    const line = lines[i]
+    // create line element
+    const item = document.createElement("div")
+    item.className = "timing-line"
+    // set item text
+    item.innerHTML = line.text.replace(/<br>/g, " ")
+    item.innerHTML = item.innerText
+    // create item outer
+    const outer = document.createElement("div")
+    outer.className = "timing-line-outer"
+    // sync event listener
+    outer.addEventListener("click", () => {
+      // return if no current time
+      if (!states.time) { return }
+      // calculate sync value
+      const amount = (states.time.current - line.from).toFixed(4)
+      // update on input
+      qs("#sync").value = amount
+      // sync message
+      sendMessage("update", { sync: amount })
+    })
+    // append item
+    outer.appendChild(item)
+    container.appendChild(outer)
+  }
+}
+
+const onTimingUpdate = time => {
+  // set current time
+  states.time = time
+  // get line elements
+  const items = qa(".timing-line")
+  // for each line
+  for (let i = 0; i < states.lines.length; i++) {
+    // current line and item
+    const line = states.lines[i]
+    const item = items[i]
+    // get current time by sync
+    const current = time.current - time.sync
+    // check line with current time
+    if (line.from <= current && line.to >= current) {
+      const amount = (current - line.from) / (line.to - line.from)
+      item.classList.add("active")
+      item.classList.remove("done")
+      item.style.boxShadow = `inset ${400 * amount}px 0px 0px 0px #000`
+    } else if (line.from < current) {
+      item.classList.add("done")
+      item.classList.remove("active")
+      item.style.boxShadow = "none"
+    } else {
+      item.classList.remove("done")
+      item.classList.remove("active")
+      item.style.boxShadow = "none"
+    }
+  }
+}
+
+const onSettings = event => {
+  // get target element
+  const target = event.target
+  // return if no value
+  if (!target.value) { return }
+  // get target key
+  const key = target.id
+  // get target value
+  const value = target.value.toLowerCase()
+  // switch by target key
+  if (key === "font-size") {
+    // update font size
+    sendMessage("update", { inner: { fontSize: parseInt(value) } })
+  } else if (key === "font-weight") {
+    // update input
+    qs("[data-weight]").setAttribute("data-weight", value)
+    // update font weight
+    sendMessage("update", { inner: { fontWeight: value } })
+  } else if (key === "background-opacity") {
+    // update background opacity
+    sendMessage("update", {
+      inner: {
+        backgroundColor: `rgba(0, 0, 0, ${value / 100})`
+      }
+    })
+  } else if (key === "spacing-x") {
+    // update spacing in x direction
+    sendMessage("update", {
+      outer: {
+        paddingLeft: parseInt(value),
+        paddingRight: parseInt(value)
+      }
+    })
+  } else if (key === "spacing-y") {
+    // update spacing in y direction
+    sendMessage("update", {
+      outer: {
+        paddingTop: parseInt(value),
+        paddingBottom: parseInt(value)
+      }
+    })
+  } else if (key === "position") {
+    // update input
+    qs("[data-position]").setAttribute("data-position", value)
+    // update position
+    sendMessage("update", {
+      outer: {
+        alignItems: value.split("-")[0],
+        justifyContent: value.split("-")[1]
+      },
+      inner: {
+        textAlign: {
+          start: "left",
+          center: "center",
+          end: "right"
+        }[value.split("-")[1]]
+      }
+    })
+  }
+}
+
+// settings event listeners
+qs("#settings").addEventListener("input", onSettings)
+qs("#settings").addEventListener("click", onSettings)
+
+// sync event listener
+qs("#sync").addEventListener("input", event => {
+  // get value
+  const value = event.target.value.toString()
+  // parse amount
+  const amount = (parseFloat(value) || 0).toFixed(4)
+  // sync message
+  sendMessage("update", { sync: amount })
+})
+
+// stop event listener
+qs("#stop").addEventListener("click", onStop)
+
+const onInit = state => {
+  // check init state
+  if (state) {
+    // request time frequently
+    setInterval(() => sendMessage("time"), 100)
+  } else {
+    document.body.setAttribute("data-unsupported", "true")
+  }
+}
+
+// get and parse current settings
+const settings = JSON.parse(localStorage.getItem("settings") || "null")
+
+// check for current settings
+if (settings) {
+  // send current settings
+  sendMessage("update", {
+    outer: settings.outer.style,
+    inner: settings.inner.style
+  }).then(onInit)
+} else {
+  // request page data
+  sendMessage("data").then(onInit)
+}
